72dcae96755399795e0cf541ad2eb311708f2f50
Josh Kenzer <jkenzer@gmail.com>
2020-10-05 17:15:00 -0700

    Animated pumpkin

--- pumpkin.js
+++ pumpkin.js
@@ -5,37 +5,50 @@ unicornHatHD.setBrightness(0.95);
 unicornHatHD.clear();
 
 let pumpkin = [
-  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' ', 'G', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
   [' ', ' ', ' ', ' ', ' ', ' ', ' ', 'G', 'G', ' ', ' ', ' ', ' ', ' ', ' ', ' '], // top
   [' ', ' ', ' ', ' ', ' ', ' ', ' ', 'G', 'G', 'G', ' ', ' ', ' ', ' ', ' ', ' '],
-  [' ', ' ', ' ', ' ', ' ', 'O', 'O', 'O', 'O', 'O', 'O', 'O', ' ', ' ', ' ', ' '],
-  [' ', ' ', ' ', ' ', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', ' ', ' ', ' '],
-  [' ', ' ', ' ', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', ' ', ' '],
-  [' ', ' ', 'O', 'O', 'O', 'Y', 'Y', 'O', 'O', 'O', 'O', 'Y', 'Y', 'O', 'O', ' '],
-  [' ', ' ', 'O', 'O', 'Y', 'Y', 'Y', 'O', 'O', 'O', 'Y', 'Y', 'Y', 'O', 'O', ' '],
-  [' ', ' ', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', ' '],
-  [' ', ' ', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', ' '],
-  [' ', ' ', 'O', 'Y', 'Y', 'Y', 'O', 'Y', 'Y', 'O', 'O', 'Y', 'Y', 'Y', 'O', ' '],
-  [' ', ' ', 'O', 'O', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'O', 'O', ' '],
-  [' ', ' ', ' ', 'O', 'O', 'Y', 'O', 'O', 'Y', 'Y', 'O', 'Y', 'O', 'O', ' ', ' '],
-  [' ', ' ', ' ', ' ', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', ' ', ' ', ' '],
-  [' ', ' ', ' ', ' ', ' ', 'O', 'O', 'O', 'O', 'O', 'O', 'O', ' ', ' ', ' ', ' '], // bottom
+  [' ', ' ', ' ', ' ', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', ' ', ' ', ' '],
+  [' ', ' ', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', ' ', ' '],
+  [' ', 'O', 'O', 'O', 'O', 'Y', 'Y', 'O', 'O', 'O', 'O', 'Y', 'Y', 'O', 'O', ' '],
+  [' ', 'O', 'O', 'O', 'Y', 'Y', 'Y', 'O', 'O', 'O', 'Y', 'Y', 'Y', 'O', 'O', ' '],
+  [' ', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', ' '],
+  [' ', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', ' '],
+  [' ', 'O', 'O', 'Y', 'Y', 'Y', 'O', 'Y', 'Y', 'O', 'O', 'Y', 'Y', 'Y', 'O', ' '],
+  [' ', 'O', 'O', 'O', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'O', 'O', ' '],
+  [' ', ' ', 'O', 'O', 'O', 'Y', 'O', 'O', 'Y', 'Y', 'O', 'Y', 'O', 'O', ' ', ' '],
+  [' ', ' ', ' ', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', ' ', ' ', ' ', ' '], // bottom
   [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
 ];
 
-for (let row = 0; row < pumpkin.length; row++) {
-  const pixels = pumpkin[row];
-  for (let p = 0; p < pixels.length; p++) {
-    if (p == "G") {
-      unicornHatHD.setPixel(row, pixel, 0, 128, 0);
-    }
-    if (p == "O") {
-      unicornHatHD.setPixel(row, pixel, 255, 165, 0);
-    }
-    if (p == "Y") {
-      unicornHatHD.setPixel(row, pixel, 255, 255, 0);
-    }
+function drawPumpkin() {
 
+  for (let row = 0; row < pumpkin.length; row++) {
+    const pixels = pumpkin[row];
+    for (let p = 0; p < pixels.length; p++) {
+      if (pixels[p] == "G") {
+        unicornHatHD.setPixel(row, p, 0, 128, 0);
+      }
+      if (pixels[p] == "O") {
+        unicornHatHD.setPixel(row, p, 204, 136, 0);
+      }
+      if (pixels[p] == "Y") {
+        unicornHatHD.setPixel(row, p, 0, 0, 0);
+      }
+      if (pixels[p] == " ") {
+        unicornHatHD.setPixel(row, p, 0, 0, 0);
+      }
+    }
   }
+  unicornHatHD.show(true, false);
+  setTimeout(() => {
+    pumpkin[6].reverse();
+    pumpkin[7].reverse();
+    pumpkin[10].reverse();
+    pumpkin[11].reverse();
+    drawPumpkin();
+  }, 550)
 }
-unicornHatHD.show(true, true);
+drawPumpkin();
